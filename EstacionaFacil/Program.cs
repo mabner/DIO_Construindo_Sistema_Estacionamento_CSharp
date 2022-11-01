79c876dc48d929a5f69e920411ec406ac52f9c3e
Marcos Leite <m.abner@live.co.uk>
2022-11-01 01:29:55 -0300

    refactor: format style

--- EstacionaFacil/Program.cs
+++ EstacionaFacil/Program.cs
@@ -7,7 +7,7 @@ decimal startingPrice = 0;
 decimal perHourRate = 0;
 
 Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-									"Digite o preço inicial:");
+                  "Digite o preço inicial:");
 startingPrice = Convert.ToDecimal(Console.ReadLine());
 
 Console.WriteLine("Agora digite o preço por hora:");
@@ -22,38 +22,38 @@ bool showMenu = true;
 // Loops the menu
 while (showMenu)
 {
-	Console.Clear();
-	Console.WriteLine("Digite a sua opção:");
-	Console.WriteLine("1 - Cadastrar veículo");
-	Console.WriteLine("2 - Remover veículo");
-	Console.WriteLine("3 - Listar veículos");
-	Console.WriteLine("4 - Encerrar");
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
 
-	switch (Console.ReadLine())
-	{
-		case "1":
-			es.AddVehicle();
-			break;
+    switch (Console.ReadLine())
+    {
+        case "1":
+            es.AddVehicle();
+            break;
 
-		case "2":
-			es.RemoveVehicle();
-			break;
+        case "2":
+            es.RemoveVehicle();
+            break;
 
-		case "3":
-			es.ListVehicles();
-			break;
+        case "3":
+            es.ListVehicles();
+            break;
 
-		case "4":
-			showMenu = false;
-			break;
+        case "4":
+            showMenu = false;
+            break;
 
-		default:
-			Console.WriteLine("Opção inválida");
-			break;
-	}
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
 
-	Console.WriteLine("Pressione uma tecla para continuar");
-	Console.ReadLine();
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
 }
 
 Console.WriteLine("O programa se encerrou");
